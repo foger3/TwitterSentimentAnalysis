14a31042c52cb4fb855c2b5579281997e21a38cf
Luca Thoms <luca.thoms@googlemail.com>
2022-05-19 15:37:37 +0200

    Added Documentation

--- twitter_nlp/__init__.py
+++ twitter_nlp/__init__.py
@@ -1,1 +1,11 @@
-from twitter_nlp.core import cool_add, ploty
+"""Using Twitter data to determine user sentiments
+
+This package uses natural language processing to determine sentiments
+of twitters users. By inputting the handle/username (@) of a certain 
+twitter user, the modules use a pre-trained model to evaluate the most 
+recent 100 tweets on the specifed account and provide a visualisation
+of the overall political sentiment, including more detailed output on
+sentiment frequency and word density measures.
+    
+"""
+from twitter_nlp.core import nlp
--- twitter_nlp/setup.py
+++ twitter_nlp/setup.py
@@ -5,6 +5,6 @@ setuptools.setup(
     version = "0.0.1",
     author = "luca",
     author_email= "luca.thoms@student.uva.nl",
-    python_requires = ">=3.6",
+    python_requires = ">=3.8",
     packages = setuptools.find_packages()
 ) 
