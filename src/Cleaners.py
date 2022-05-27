2c1e95249c00f2549b3b73c20adbfc8123494b7a
Luca Thoms <luca.thoms@googlemail.com>
2022-05-27 21:43:39 +0200

    Tailored to Style Guidelines

--- src/Cleaners.py
+++ src/Cleaners.py
@@ -7,13 +7,13 @@ from nltk.corpus import stopwords
 
 class Classifier:
     this_dir, this_filename = os.path.split(__file__) 
-    data_path = os.path.join(this_dir, 'model/political_classifier.pickle')
+    data_path = os.path.join(this_dir, 'model/pol_classifier.pickle') 
 
 class Cleaners(Classifier):
     
     def __init__(self):
         model_hold = open(Cleaners.data_path, 'rb')
-        self.Classifier = pickle.load(model_hold)
+        self.classifier = pickle.load(model_hold)
         model_hold.close()
 
     def clean_tweets(self, tweet_text, tweet_date):
@@ -22,27 +22,30 @@ class Cleaners(Classifier):
             tweet_cleaned.append(self.remove_noise(word_tokenize(tweet_text[tweet[0]])))
 
         tweet_df = pd.DataFrame({'tweets': tweet_cleaned, 'dates': tweet_date})
-        tweet_df = tweet_df[tweet_df.astype(str)['tweets'] != '[]']
+        tweet_df = tweet_df[tweet_df.astype(str)['tweets'] != '[]']    
         return tweet_df
     
-    def clean_tweets_sentiment(self, tweet_df):        
+    def clean_sentiment(self, tweet_df):        
         tweet_sentiments = []
         for tweet in tweet_df.tweets.tolist():
-            tweet_sentiments.append(self.Classifier.classify(dict([token, True] for token in tweet))) 
+            tweet_sentiments.append(self.classifier.classify(dict([token, True] for token in tweet))) 
         tweet_sentiments = tweet_sentiments
         
         tweet_prob_sentiments = []
         for tweet in tweet_df.tweets.tolist():
-            dist = self.Classifier.prob_classify(dict([token, True] for token in tweet))
+            dist = self.classifier.prob_classify(dict([token, True] for token in tweet))
             for label in dist.samples():
                 tweet_prob_sentiments.append(dist.prob(label))  
         tweet_prob_sentiments = [x for x in tweet_prob_sentiments if x > 0.5]
         tweet_prob_sentiments = [x - 0.5 for x in tweet_prob_sentiments]
 
-        tweet_sentiments_df = pd.DataFrame({'sentiments': tweet_sentiments, 'sentiments_prob': tweet_prob_sentiments, 'dates': tweet_df.dates})
+        tweet_sentiments_df = pd.DataFrame(
+            {'sentiments': tweet_sentiments, 
+            'sentiments_prob': tweet_prob_sentiments, 
+            'dates': tweet_df.dates})
         tweet_sentiments_df.loc[tweet_sentiments_df['sentiments'].str.contains('Conservative'), 'sentiments_prob'] *= -1
         tweet_sentiments_df = tweet_sentiments_df.sort_values(by = 'dates')
-        tweet_sentiments_df['14day_run_avg'] = tweet_sentiments_df.sentiments_prob.rolling(14).mean()
+        tweet_sentiments_df['14_run_avg'] = tweet_sentiments_df.sentiments_prob.rolling(14).mean()
         return tweet_sentiments_df
 
     def remove_noise(self, tweet_tokens):
@@ -50,8 +53,8 @@ class Cleaners(Classifier):
         cleaned_tokens = []
         for token, tag in pos_tag(tweet_tokens):
             token = re.sub(r'[0-9]', '', token) # remove numbers
-            token = re.sub('[^A-Za-z0-9]+', '', token) # remove special chr
-            token = re.sub(r'\b\w{1,2}\b', '', token) # remove words < 3chr     
+            token = re.sub('[^A-Za-z0-9]+', '', token) # remove special
+            token = re.sub(r'\b\w{1,2}\b', '', token) # remove words<3c     
             token = re.sub(r'\bhtt\w+', '', token) # remove links
             token = re.sub(r'\btco\w+', '', token) # remove links
 
--- src/Visuals.py
+++ src/Visuals.py
@@ -13,52 +13,85 @@ class Visuals(Cleaners.Classifier):
 
     def __init__(self):
         model_hold = open(Visuals.data_path, 'rb')
-        self.Classifier = pickle.load(model_hold)
+        self.classifier = pickle.load(model_hold)
         model_hold.close()
         self.clean = Cleaners.Cleaners()
 
     def word_density(self, tweet_cleaned):
-        plt.figure(1)
         fig, ax = plt.subplots(figsize=(10, 6))
         freq_words = FreqDist(self.clean.get_all_words(tweet_cleaned))
-        filter_words = dict([(m, n) for m, n in freq_words.items() if len(m) > 3])
+        filter_words = dict(
+            [(m, n) for m, n in freq_words.items() if len(m) > 3]
+            )
         cloud = WordCloud().generate_from_frequencies(filter_words)
         ax.imshow(cloud, interpolation = 'bilinear')
         ax.axis("off")
-        #plt.show()    
+        plt.show()    
 
     def sentiment_plots_pie(self, pie_chart_data):
-        plt.figure(2)
         fig, ax = plt.subplots(figsize = (8, 6))
         fig.patch.set_facecolor('white')
         patches, texts, pcts = ax.pie(
-            pie_chart_data, labels= ['Liberal', 'Conservative'], 
-            autopct = '%.1f%%', startangle = 90, 
+            pie_chart_data, 
+            autopct = lambda p:'{:.1f}%'.format(p) if p > 0 else '', 
+            startangle = 90, 
             wedgeprops={'linewidth': 1, 'edgecolor': 'black'},
-            textprops={'size': 'x-large'}, explode = [0.2, 0])
-        plt.setp(pcts, color = 'white', fontweight = 'bold')
+            textprops={'size': 'x-large'}, 
+            explode = [0.2 if (pie_chart_data[0] > 0 and \
+                    pie_chart_data[1] > 0) else 0, 0]
+            )
+        ax.legend(
+            labels = ['Liberal', 'Conservative'], 
+            loc = 'lower right'
+            )
         ax.set_title('Pie Chart of Sentiment Ratio', fontsize = 18)
-        #plt.show()
+        plt.setp(pcts, color = 'white', fontweight = 'bold')
+        plt.show()
         
     def sentiment_plots_time(self, time_series_data):
-        plt.figure(3)
-        xtick_locator = mdates.AutoDateLocator(interval_multiples = False)
-        xtick_formatter = mdates.AutoDateFormatter(xtick_locator)
         fig, ax = plt.subplots(figsize=(12, 6))
-        ax.plot(time_series_data.dates, time_series_data.sentiments_prob, color = "grey")
-        sns.lineplot( x = 'dates', y = '14day_run_avg', data = time_series_data, color = "darkorange", linewidth = 2)
+        ax.plot(
+            time_series_data.dates, 
+            time_series_data.sentiments_prob, 
+            color = "grey"
+            )
+        sns.lineplot(
+            x = 'dates', 
+            y = '14_run_avg', 
+            data = time_series_data, 
+            color = "darkorange", 
+            linewidth = 2, 
+            label = "14 Tweet Running Average"
+            )
+        xtick_locator = mdates.AutoDateLocator(
+            interval_multiples = False
+            )
+        xtick_formatter = mdates.AutoDateFormatter(
+            xtick_locator
+            )        
         ax.set_xlabel('Date (in months)', fontsize = 14)
         ax.xaxis.set_major_locator(xtick_locator)
         ax.xaxis.set_major_formatter(xtick_formatter)
+        ax.set_ylabel("")
         ax.set_yticks(np.arange(-0.5, 0.6, 0.25))
-        ax.set_yticklabels(labels = ['', 'Conservative', '', 'Liberal', ''], fontsize = 12, rotation = 90, va = 'center')
+        ax.set_yticklabels(
+            labels = ['', 'Conservative', '', 'Liberal', ''], 
+            fontsize = 12, 
+            rotation = 90, 
+            va = 'center'
+            )
+        ax.set_ylim(-0.6, 0.6)
         ax.set_title('Tweet Sentiment Over Time', fontsize = 18)
         fig.autofmt_xdate(rotation = 20, ha = 'center')
         plt.show()
 
     def single_tweet(self, tweet_text, num):
-        single_tweet_token = self.clean.remove_noise(word_tokenize(tweet_text[num - 1]))
-        single_tweet = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
-                            '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', tweet_text[num - 1])
+        single_tweet_token = self.clean.remove_noise(
+            word_tokenize(tweet_text[num - 1])
+            )
+        single_tweet = re.sub(
+            'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+            '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', tweet_text[num - 1]
+            )
         print(f'The following Tweet: \n\n"{single_tweet.strip()}"')
-        print(f'\nHas been classified as: "{self.Classifier.classify(dict([token, True] for token in single_tweet_token))}"')
+        print(f'\nHas been classified as: "{self.classifier.classify(dict([token, True] for token in single_tweet_token))}"')
